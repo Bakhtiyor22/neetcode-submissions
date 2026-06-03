cb5c60a264dac23fdcb28b11509140fc44851064
Bakhtiyor <99182103+Bakhtiyor22@users.noreply.github.com>
2026-06-03 17:00:42 +0500

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while(i < n){
+            if(nums[i] == val){
+                swap(nums, i, n-1);
+                n--;
+            } else i++;
+        }
+
+
+        return n;
+    }
+
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
